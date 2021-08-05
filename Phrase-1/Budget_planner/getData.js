475111a26de317283f5d105d7a4591e243a40095
Ikenna Ibezim <45284274+iamikenna@users.noreply.github.com>
2021-08-05 11:27:32 -0400

    Display data on table

--- Phrase-1/Budget_planner/getData.js
+++ Phrase-1/Budget_planner/getData.js
@@ -1,30 +1,57 @@
 
-function getD(){
-    var clientN = document.getElementById("cn").value;
-    console.log(clientN)
+function getNewKey(){
+	let getObj = localStorage.getItem("data");
+	let getJson = JSON.parse(getObj);
+	let large = 1
 
-    var myPTag = document.createElement("p");
-
-    var myPTagContent = document.createTextNode(clientN);
-
-    myPTag.appendChild(myPTagContent);
-
-    document.getElementById("info").appendChild(myPTag);
+	if (getJson !== undefined || getJson !== null){
+		try{
+			for (const [key, value] of Object.entries(getJson)){
+				if (key > large){
+					large = key
+				}
+			}
+			return large + 1
+		}
+		catch(err){
+			console.log(err)
+		}
+	}
+	return 1
 }
 
 function setD(){
-    const map1 = {};
-    var clientN = document.getElementById("cn").value;
-    var projectN = document.getElementById("pn").value;
-    var bgtN = document.getElementById("bgt").value;
+	// Empty Object
+	
 
-    map1[1] = clientN;
-    map1[2] = projectN;
-    map1[3] = bgtN;
+	// This gets values from the input form
+	var clientN = document.getElementById("cn").value;
+	var projectN = document.getElementById("pn").value;
+	var bgtN = document.getElementById("bgt").value;
 
-    localStorage.setItem("data", JSON.stringify(map1));
-    console.log("Data has been stored in the local storage")
+	var map1 = JSON.parse(localStorage.getItem("data")) || {};
+
+	map1[getNewKey()] = {"clientN": clientN, "projectN": projectN, "bgtN": bgtN}
+	localStorage.setItem("data", JSON.stringify(map1))
+}
+
+
+function displayT(){
+	let getObj = localStorage.getItem("data");
+	let getJson = JSON.parse(getObj);
+	var tableContent = "";
+
+	
+	var startTable = "<table border=1 class='table'><tr><th>Client Name</th><th>Project Name</th><th>Budget</th></tr>"
+	for (const [key, value] of Object.entries(getJson)){
+		tableContent +="<tr><td>"+value['clientN']+"</td><td>"+value["projectN"]+"</td><td>"+value["bgtN"]+"</td></tr>"
+	}
+	
+
     
+    var endTable="</table>"
 
-    
+    tableContent = startTable+tableContent+endTable
+
+    document.getElementById("main").innerHTML=tableContent;
 }
